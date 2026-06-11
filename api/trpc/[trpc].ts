657e708a9090b05e00d9229b482090e31d09064a
pecinha12 <cleitin.duh.grau.777@gmail.com>
2026-06-11 19:54:16 -0300

    fix api

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,7 +1,12 @@
-import * as trpcExpress from "@trpc/server/adapters/express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { appRouter } from "../../server/routers";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default trpcExpress.createExpressMiddleware({
+const handler = createExpressMiddleware({
   router: appRouter,
   createContext: ({ req, res }: any) => ({ req, res, user: null }),
 });
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  return handler(req as any, res as any, () => {});
+}
