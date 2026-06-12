75887488bbdc5ffa8edfe01ac10fe1ffd71caa4f
pecinha12 <cleitin.duh.grau.777@gmail.com>
2026-06-11 23:27:39 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { trpc } from "@/lib/trpc";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import { httpLink } from "@trpc/client";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 
 const trpcClient = trpc.createClient({
   links: [
-    httpBatchLink({
+    httpLink({
       url: "/api/trpc",
       fetch(input, init) {
         return globalThis.fetch(input, {
